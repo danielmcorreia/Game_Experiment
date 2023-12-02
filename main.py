1a6243da9ae7de67a44856837be88d8a61de30aa
danielmcorreia <52961864+danielmcorreia@users.noreply.github.com>
2023-12-02 20:59:04 +0000

    feat/parameters_file

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import yaml
 import arcade
 
 def read_yaml_parameters(file_path):
-    print('Chegou aqui')
     with open(file_path, 'r') as file:
         parameters = yaml.safe_load(file)
     return parameters
 
-# Example usage
 yaml_parameters = read_yaml_parameters('parameters.yaml')
-print('oi')
 print(yaml_parameters)
